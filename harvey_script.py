72ba7949348c077892933625d338867f8f1613b2
jli452 <jli452@binghamton.edu>
2020-05-20 22:57:50 -0400

    wont comment on itself anymore

--- harvey_script.py
+++ harvey_script.py
@@ -1,5 +1,7 @@
 import praw
 import config
+import os
+import time
 
 def bot_login():
     print("Logging in")
@@ -11,16 +13,29 @@ def bot_login():
     print("Logged in using this bot: " + config.username)
     return r
 
-def run_bot(r):
-    num_commented = 0
-    for comment in r.subreddit('BinghamtonUniversity').comments(limit = 100):
-        if "Harvey" in comment.body:
-            comment.reply("Harvey G. Stenger is an American educator and academic administrator, who is serving as the seventh president of Binghamton University since 2012.")
-            num_commented += 1
-            print(str(num_commented) + " comments replied to")
+def run_bot(r, comments_replied):
+    for comment in r.subreddit('tifu').comments(limit = 25):
+        if "you" in comment.body and comment.id not in comments_replied and comment.author != r.user.me():
+            comment.reply("good bot")
+            #comment.reply("Harvey G. Stenger is an American educator and academic administrator, who is serving as the seventh president of Binghamton University since 2012.")
+            comments_replied.append(comment.id)
+            print("a comment has been replied to in this session")
+            with open("comments_replied.txt", "a") as f:
+                f.write(comment.id + "\n")
+            time.sleep(.5)
 
-def main():
-    r = bot_login()
-    run_bot(r)
+def get_saved_comments():
+    if not os.path.isfile("comments_replied.txt"):
+        comments_replied = []
+    else:
+        with open("comments_replied.txt", "r") as f:
+            comments_replied = f.read()
+            comments_replied = comments_replied.split("\n")
+            comments_replied = list(filter(None, comments_replied))
+    return comments_replied
 
-main()
+r = bot_login()
+comments_replied = get_saved_comments()
+
+while True:
+    run_bot(r, comments_replied)
