aad92bfd9053fd4f314b49fcb734ef91bb1a7024
Jin Xian Li <jli452@binghamton.edu>
2022-03-15 02:32:56 -0400

    email pw not working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import praw
 import config
 import os
 import time, datetime
+import smtplib, ssl
 
 def bot_login():
     print("Logging in")
@@ -34,16 +35,29 @@ def text_to_dict(textfile):
     f.close()
     return titles
 
-def run_bot(r, sr):
+def email(sender_email, receiver_email, message, password):
+    port = 465
+    smtp_server = "smtp.gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+def run_bot(r, sr, pw, se, re):
     for post in r.subreddit(sr).new(limit = 25):
         if post.title not in text_to_dict("titles.txt"):
             write_title(post.title)
+            message = "New post to " + sr + ":" + post.title
+            email(se, re, pw, message)
 
 def main():
     r = bot_login()
     sr = input("Type in the subreddit you want to track:\n")
+    se = input("Type sender email:\n")
+    re = input("Type receiver email:\n")
+    pw = input("Type your password for email that you want to receive new notifications for:\n")
     while(1):
-        run_bot(r, sr)
+        run_bot(r, sr, pw, se, re)
 
 if __name__ == "__main__":
     main()
